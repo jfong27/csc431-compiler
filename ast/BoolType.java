f2e3d070c3481d07a2c848eaf4258dba3e444117
Brent Lee <brenthlee@gmail.com>
2019-04-08 13:06:00 -0700

    merged changes

--- ast/BoolType.java
+++ ast/BoolType.java
@@ -3,4 +3,11 @@ package ast;
 public class BoolType
    implements Type
 {
+    public boolean equals(Type t) {
+        if (t instanceof BoolType) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- ast/IntType.java
+++ ast/IntType.java
@@ -3,4 +3,11 @@ package ast;
 public class IntType
    implements Type
 {
+    public boolean equals(Type t) {
+        if (t instanceof IntType) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
